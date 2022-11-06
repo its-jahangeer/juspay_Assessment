d521c468041eb9fa8609c19ad4794168879b0dbc
dev_Buzzart <jahangeer.khan.buzzartsoftware@gmail.com>
2022-11-07 03:35:22 +0530

    active functionality added

--- src/screens/editorscreen/dragActions.js
+++ src/screens/editorscreen/dragActions.js
@@ -1,7 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {SafeAreaView, Text, Image,View, TouchableOpacity} from 'react-native';
 import Material from "react-native-vector-icons/MaterialCommunityIcons";
+
 const DragActions = () => {
+  const [isActive, setActive]=useState(false);
+
+  const HandleClick = ()=>{
+    setActive(true);
+  }
+  const HandleClick2 = ()=>{
+    setActive(false);
+  }
   return (
     <>
     <View style={{justifyContent:'center',borderBottomWidth:0.8,flexDirection:'row'}}>
@@ -9,10 +18,10 @@ const DragActions = () => {
         <Text style={{textTransform:'uppercase',fontSize:16,color:'green',fontWeight:'800'}}>Action</Text>
       </View>
       <View style={{flexDirection:'row',justifyContent:'space-around'}}>
-      <TouchableOpacity style={{backgroundColor:'grey',flex:1}}>
+      <TouchableOpacity onPress={HandleClick} style={{backgroundColor:isActive?'green':'grey',flex:1}}>
         <Text style={{color:"white",fontSize:16,textAlign:'center'}}>Action1</Text>
       </TouchableOpacity>
-      <TouchableOpacity style={{backgroundColor:'grey',flex:1}}>
+      <TouchableOpacity onPress={HandleClick2} style={{backgroundColor:isActive?'grey':'green',flex:1}}>
         <Text style={{color:"white",fontSize:16,textAlign:'center'}}>Action2</Text>
       </TouchableOpacity>
       </View>
