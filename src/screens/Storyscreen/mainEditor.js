2c9d789d61f34c183c28c3531a0e85e3796d8fb1
Ajit Roul <ajitroul@Ajits-MacBook-Pro.local>
2022-11-10 05:27:09 +0530

    list implemented:

--- src/screens/Storyscreen/mainEditor.js
+++ src/screens/Storyscreen/mainEditor.js
@@ -7,18 +7,22 @@ import Listscreen from './ActionCards';
 import TopBar from '../../common/topbar';
 import Draggable from 'react-native-draggable';
 import Axis from './Axis';
-import { useContext, useEffect } from 'react/cjs/react.development';
+import { useContext } from 'react/cjs/react.development';
 import ActionContext from '../../store/action-context';
+import functionList from '../../utils/actions';
 
 
 const MainEditor = (props) => {
 
   const ctx = useContext(ActionContext);
 
-  useEffect(()=> {
-    console.log("--------")
-    console.log(ctx.selectedActionList);
-  },[])
+  const play = () => {
+    const selectedList = ctx.selectedActionList;
+    selectedList.forEach(item => {
+      const fnName = item.id;
+      functionList[fnName]();
+    });
+  }
   return (
     <>
     <TopBar/>
@@ -26,7 +30,8 @@ const MainEditor = (props) => {
         <View style={{margin:7,backgroundColor:'white',height:'60%'}}>
         <Draggable imageSource={Images.Cat} x={75} y={100}  renderSize={120} onDragRelease={(event)=>alert(event.left)}/> 
 
-        <TouchableOpacity style={{backgroundColor:'#5394fc',width:40,height:40,borderRadius:40/2,justifyContent:'center',position:"absolute",bottom:11,right:8}}>
+        <TouchableOpacity style={{backgroundColor:'#5394fc',width:40,height:40,borderRadius:40/2,justifyContent:'center',position:"absolute",bottom:11,right:8}}
+          onPress ={play}>
            <Material name="play" size={30} color="#fff" style={{alignSelf:'center'}} />
          </TouchableOpacity>
          
---
+++ src/utils/actions.js
@@ -0,0 +1,35 @@
+export default functionList = {
+    moveX50: () => {
+        console.log('moveX50')
+    },
+    moveY50: () => {
+        console.log('moveY50')
+    },
+    rotate360: () => {
+        console.log('rotate360')
+    },
+    goToOrigin: () => {
+        console.log('goToOrigin')
+    },
+    goToMiddle: () => {
+        console.log('goToMiddle')
+    },
+    goToRandom: () => {
+        console.log('goToRandom')
+    },
+    sayHello: () => {
+        console.log('sayHello')
+    },
+    sayHelloWithTime: () => {
+        console.log('sayHelloWithTime')
+    },
+    increaseSize: () => {
+        console.log('increaseSize')
+    },
+    decSize: () => {
+        console.log('decSize')
+    },
+    repeat: () => {
+        console.log('repeat')
+    }
+}
